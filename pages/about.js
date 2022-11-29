f669836ac7069fbc25a700f4cf8de52ff415e09d
Franco Arrano Silva <farrano@kdu.cl>
2022-11-29 13:13:21 -0300

    Agrega componentes de Navbar y footer, fotografia de prueba

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+// import head component
+import Head from "next/head";
+
+export default function About() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Soy adri
+      </h1>
+    );
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+// import head component
+import Head from "next/head";
+import destacada from '../public/1.png'
+import Image from 'next/image'
+
 export default function Home() {
     return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
+      <><h1 className="text-3xl font-bold underline">
+        Parcelas y Terrenos
+      </h1><Image
+          src={destacada}
+          alt="Picture of the author" /></>
+    );
   }
