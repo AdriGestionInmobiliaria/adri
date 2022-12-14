bd29e96ebf2cc364ab8aea0437403f14ffa0d9bb
Franco Arrano Silva <farrano@kdu.cl>
2022-12-14 16:59:13 -0300

    Cambio de colores en footer

--- Components/footer.js
+++ Components/footer.js
@@ -2,13 +2,13 @@ import adri_logo from '../public/adri.png'
 import Image from 'next/image'
 export default function Footer() {
   return (
-    <div className="flex flex-row bg-yellow-800  px-2 sm:px-4 py-2.5 rounded">
+    <div className="flex flex-row bg-stone-900  px-2 sm:px-4 py-2.5 rounded">
       <div className="container flex flex-wrap items-center mx-auto justify-between ">
         <Image src={adri_logo} alt="Adri Gestion Inmobiliaria" className="logo-img-footer" />
-        <div className="font-mono fonts text-l">
+        <div className="font-mono fonts text-l  text-neutral-50">
           <h2>Adri Gestion Inmobiliaria</h2>
         </div>
-        <div id="telefono" className="font-mono text-l">
+        <div id="telefono" className="font-mono text-l text-neutral-50">
           <h2>+56964947609</h2>
         </div>
       </div>
