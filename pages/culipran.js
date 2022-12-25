a2b801b5569c25d12d83fc8080198ad8594774cf
Franco Arrano Silva <farrano@kdu.cl>
2022-12-25 17:50:36 -0300

    Nuevo home

---
+++ pages/culipran.js
@@ -0,0 +1,75 @@
+// import head component
+import culipran1 from '../public/culipran/1.png'
+import culipran2 from '../public/culipran/2.png'
+import culipran3 from '../public/culipran/3.png'
+import culipran4 from '../public/culipran/4.png'
+import culipran5 from '../public/culipran/5.png'
+import culipran6 from '../public/culipran/6.png'
+import culipran7 from '../public/culipran/7.png'
+import culipran8 from '../public/culipran/8.png'
+import culipran9 from '../public/culipran/9.png'
+
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Navigation } from "swiper";
+
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/navigation";
+
+export default function Culipran() {
+  return (
+    <>
+      <div className="font-serif subpixel-antialiased">
+        <div className="p-4 text-black bg-gray-200" >
+          <div className=" px-6 py-6 flex-col">
+            <h1 className="text-2xl">Parcelas Estero el Yali, Santo Domingo</h1>
+          </div>
+          <div className="px-6 flex-col">
+            <Swiper navigation={true} modules={[Navigation]}>
+              <SwiperSlide>
+                <Image src={culipran1} alt="Parcela Melipilla"/>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src={culipran2} alt="Parcela San antonio" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src={culipran3} alt="Parcela San antonio" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src={culipran4} alt="Parcela San antonio" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src={culipran5} alt="Parcela San antonio" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src={culipran6} alt="Parcela San antonio" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src={culipran7} alt="Parcela San antonio" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src={culipran8} alt="Parcela San antonio" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src={culipran9} alt="Parcela San antonio" />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+          <div className="px-6 flex-col">
+            <ul class="list-disc p-4 px-6">
+              <li>Sector: Agua Santa</li>
+              <li>5.000 m²</li>
+              <li>Rol propio</li>
+              <li>Factibilidad de agua y luz</li>
+              <li>A 20 min de Melipila</li>
+              <li>Crédito directo</li>
+              <li>Pie desde $10.000.000</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,40 @@
-// import head component
-import culipran1 from '../public/culipran/1.png'
-import culipran2 from '../public/culipran/2.png'
-import culipran3 from '../public/culipran/3.png'
-import culipran4 from '../public/culipran/4.png'
-import culipran5 from '../public/culipran/5.png'
-import culipran6 from '../public/culipran/6.png'
-import culipran7 from '../public/culipran/7.png'
-import culipran8 from '../public/culipran/8.png'
-import culipran9 from '../public/culipran/9.png'
-
-import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react';
-// import required modules
-import { Navigation } from "swiper";
-
-// Import Swiper styles
-import 'swiper/css';
-import "swiper/css/navigation";
+import Card from "../Components/card";
+import Link from "next/link";
 
 export default function App() {
   return (
     <>
-      <div className="font-mono">
-        <div className="p-4 text-black bg-lime-50" >
-          <div className=" px-6 py-6 flex-col">
-            <h1 className="text-2xl">Parcelas Estero el Yali, Santo Domingo</h1>
-          </div>
-          <div className="px-6 flex-col">
-            <Swiper navigation={true} modules={[Navigation]}>
-              <SwiperSlide>
-                <Image src={culipran1} alt="Parcela Melipilla"/>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={culipran2} alt="Parcela San antonio" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={culipran3} alt="Parcela San antonio" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={culipran4} alt="Parcela San antonio" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={culipran5} alt="Parcela San antonio" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={culipran6} alt="Parcela San antonio" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={culipran7} alt="Parcela San antonio" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={culipran8} alt="Parcela San antonio" />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image src={culipran9} alt="Parcela San antonio" />
-              </SwiperSlide>
-            </Swiper>
-          </div>
-          <div className="px-6 flex-col">
-            <ul class="list-disc p-4 px-6">
-              <li>Sector: Agua Santa</li>
-              <li>5.000 m²</li>
-              <li>Rol propio</li>
-              <li>Factibilidad de agua y luz</li>
-              <li>A 20 min de Melipila</li>
-              <li>Crédito directo</li>
-              <li>Pie desde $10.000.000</li>
-            </ul>
-          </div>
+      <div className="flex">
+        <div className="px-6 py-6 mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-900">
+          <h1>Parcelas en venta</h1>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap">
+        <div className="px-6 py-6">
+          <Link href={`/culipran`}>
+            <Card title="Parcela Culipran" description={"Soy una descripcion de una parcela, esta es la segunda linea de la descripcion"} />
+          </Link>
+        </div>
+        <div className="px-6 py-6">
+          <Link href={`/culipran`}>
+            <Card title="Parcela Culipran" description={"Soy una descripcion de una parcela, esta es la segunda linea de la descripcion"} />
+          </Link>
+        </div>
+        <div className="px-6 py-6">
+          <Link href={`/culipran`}>
+            <Card title="Parcela Culipran" description={"Soy una descripcion de una parcela, esta es la segunda linea de la descripcion"} />
+          </Link>
+        </div>
+        <div className="px-6 py-6">
+          <Link href={`/culipran`}>
+            <Card title="Parcela El Yali" description={"Soy una descripcion de una parcela, esta es la segunda linea de la descripcion"} />
+          </Link>
+        </div>
+        <div className="px-6 py-6">
+          <Link href={`/culipran`}>
+            <Card title="Parcela" description={"Soy una descripcion de una parcela, esta es la segunda linea de la descripcion"} />
+          </Link>
         </div>
       </div>
     </>
